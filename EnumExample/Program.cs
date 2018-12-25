27f04f246e1981958ff9b92132180a2ad632b1a3
vinodkumartony <vinodkumar.tony503@live.com>
2018-12-25 13:15:55 +0530

    enum example added

--- EnumExample/Program.cs
+++ EnumExample/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace EnumExample
 {
+	public enum ShppingMethod
+	{
+		RegularAirMal = 1,
+		RegisteredAirMal = 2,
+		Express = 3
+
+	}
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			var method = ShppingMethod.Express;
+			Console.WriteLine((int)method);
+			
+
+			var methodId = 3;
+			Console.WriteLine((ShppingMethod)methodId);
+
+			var methodName = "Express";
+			var shippingMethod = (ShppingMethod)Enum.Parse(typeof(ShppingMethod), methodName);
+			Console.WriteLine(shippingMethod);
+			Console.ReadLine();
 		}
 	}
 }
